be01cb88d89bd131e6b649a5fafdec25ac7c43c4
k2works <kakimomokuri@gmail.com>
2019-11-12 14:17:47 +0900

    test: 受け入れテスト修正

--- cypress/integration/fizz_buzz_page_spec.js
+++ cypress/integration/fizz_buzz_page_spec.js
@@ -1,26 +1,38 @@
-describe("FizzBuzz Page", function() {
-  it("successfully loads", function() {
+describe("FizzBuzz Page", () => {
+  it("successfully loads", () => {
     cy.visit("http://localhost:8080"); // change URL to match your dev URL
     cy.get("h2").should("contain", "FizzBuzz");
   });
 
-  describe("タイプ1を選択した場合", function() {
-    it("通常パターンが表示される", function() {
-      cy.get('tbody > :nth-child(1) > :nth-child(3)').should("contain","Fizz")
+  describe("一覧編集画面", () => {
+    beforeEach(() => {
+      cy.get('#tab-menu02').click();
     });
-  });
 
-  describe("タイプ2を選択した場合", function() {
-    it("数字のみのパターンが表示される", function() {
-      cy.get('#fizz-buzz-app-select-type').select('タイプ2')
-      cy.get('tbody > :nth-child(1) > :nth-child(3)').should("contain","3")
+    describe("タイプ1を選択した場合", () => {
+      it("通常パターンが表示される", () => {
+        cy.get("tbody > :nth-child(1) > :nth-child(3)").should(
+          "contain",
+          "Fizz"
+        );
+      });
     });
-  });
 
-  describe("タイプ3を選択した場合", function() {
-    it("FizzBuzzのみのパターンが表示される", function() {
-      cy.get('#fizz-buzz-app-select-type').select('タイプ3')
-      cy.get('tbody > :nth-child(2) > :nth-child(5)').should("contain", "FizzBuzz")
+    describe("タイプ2を選択した場合", () => {
+      it("数字のみのパターンが表示される", () => {
+        cy.get("#fizz-buzz-app-select-type").select("タイプ2");
+        cy.get("tbody > :nth-child(1) > :nth-child(3)").should("contain", "3");
+      });
+    });
+
+    describe("タイプ3を選択した場合", () => {
+      it("FizzBuzzのみのパターンが表示される", () => {
+        cy.get("#fizz-buzz-app-select-type").select("タイプ3");
+        cy.get("tbody > :nth-child(2) > :nth-child(5)").should(
+          "contain",
+          "FizzBuzz"
+        );
+      });
     });
   });
 });
