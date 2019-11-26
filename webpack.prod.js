e611d586f643f6396a2ad062f22efd704e812857
k2works <kakimomokuri@gmail.com>
2019-11-26 10:51:21 +0900

    chore: PluntUML対応

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -52,6 +53,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "./contents.css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new CopyWebpackPlugin([{ from: "src/page/lib", to: "lib" }])
   ]
 };
