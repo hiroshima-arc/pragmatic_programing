e727366784ac5fb46f850dec7287e2a75ff3407f
k2works <kakimomokuri@gmail.com>
2019-12-03 18:00:41 +0900

    test: 受け入れテストセットアップ

---
+++ cypress/integration/money_page.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+describe('The Money Example', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:8080') // change URL to match your dev URL
+  })
+
+  describe('レポート', () => {
+    it('CSVファイルをアップロードする', () => {
+    })
+
+    it('レポートが表示される', () => {
+    })
+
+    it('CSVファイルをダウンロードする', () => {
+    })
+  })
+
+  describe('為替レート', () => {
+    it('為替レートを追加する', () => {
+    })
+
+    it('為替レートを更新する', () => {
+    })
+
+    it('為替レートを削除する', () => {
+    })
+
+    it('為替レートを全て削除する', () => {
+    })
+  })
+})
