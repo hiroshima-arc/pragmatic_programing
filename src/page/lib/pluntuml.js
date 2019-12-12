0880a2efe235c983ffc32eb79282d8036803b9c3
k2works <kakimomokuri@gmail.com>
2019-12-12 17:48:12 +0900

    docs: 設計ドキュメント作成

--- src/page/lib/pluntuml.js
+++ src/page/lib/pluntuml.js
@@ -1,185 +1,52 @@
 /* eslint-disable */
+const usecaseDiagram = (() => {
+  const inputId = "usecase-diagram-input";
+  const outputId = "usecase-im";
+  const source = `
+left to right direction
+skinparam packageStyle rectangle
+actor customer
+rectangle VedioRental {
+  customer -- (Rental)
+              (Calculate rental fee)
+              (Print a statement)
+}
+          `;
+  compress(source, outputId);
+})();
+
 const classDiagram = (() => {
   const inputId = "class-diagram-input";
   const outputId = "class-im";
   const source = `
-class Expression {
-  times(multiplier)
-  plus(addend)
-  reduce(bank, to)
+class Movie {
+  priceCode:int
 }
-class Sum {
-  augend: Expression
-  addend: Expression
-  times(multiplier): Expression
-  reduce(to): Money
-  plus(added): Expression
+class Rental {
+  daysRented:int
 }
-class Bank {
-  rates: Map
-  reduce(source, to): Money
-  addRate(from, to, rate): void
-  rate(from, to): int
+class Customer {
+  statement()
 }
-class Pair {
-  from: String
-  to: String
-  equlas(object): boolean
-  hashCode(): int
-  toString(): String
-}
-class Money {
-  amount: int
-  currency() :String
-  times(multiplier) :Money
-  plus(addend) :Expression
-  equals(object) :boolean
-  reduce(to) :Money
-  {static} dollar(amount): Dollar
-  {static} franc(amount): Franc
-}
-class ExChangeRate {
-  from: String
-  to: String
-  rate: float
-  id: int
-}
-class Report {
-  title: String
-  sum: int
-  currenty: String
-  items<ReportLineItem>: List
-  total(): Sum
-}
-class ReportLineItem {
-  stockName: String
-  stockAmount: int
-  price: Money
-  sum: Money
-}
-class ExChangeRates {
-  record: List<ExChangeRate>
-}
-class MoneyService {
-  setupDb(): void
-  createReportViewModel(data): RepositoryViewModel
-  addExChangeRate(entity): ExChangeRates
-  selectAllExChangeRate(): ExChangeRates
-  updateExChangeRate(id):  ExChangeRates
-  deleteExChangeRate(id): ExChangeRates
-  deleteAllExChangeRate(): ExChangeRates
-  saveReport(): void
-  getReport(): ReportViewModle
-  deleteReport(): void
-}
-class ReportService {
-  repository: ReportRepository
-  createReportViewModel(data): RepositoryViewModel
-  saveReport(): void
-  getReport(): ReportViewModle
-  deleteReport(): void
-}
-class ExChangeRateService {
-  repository: ExChangeRateRepository
-  addExChangeRate(entity): ExChangeRates
-  selectAllExChangeRate(): ExChangeRates
-  updateExChangeRate(id):  ExChangeRates
-  deleteExChangeRate(id): ExChangeRates
-  deleteAllExChangeRate(): ExChangeRates
-}
-class MoneyDB {
-  name: String
-  {static}REPORT
-  {static}EXCHANGE_RATS
-  setup(): void
-  connect(): void
-}
-class ReportRepository {
-  table: String
-  setup(): void
-  connect(): void
-  get(): ReporViewModel
-  save(): void
-  destroy(): void
-}
-class ExChangeRateRepository {
-  table: String
-  setup(): void
-  connect(): void
-  create(): ExChangeRate
-  createBatch(): List<ExChangeRate>
-  save(): ExChangeRate
-  find(): ExChangeRate
-  seletAll(): List<ExChangeRate>
-  delete(): void
-  destroy(): void
-}
-class MoneyView {
-  REPORT: 1
-  EXCHANGE: 2
-  render(): String
-}
-class ReportView {
-  render(): String
-}
-class ExChangeRateView {
-  render(): String
-}
-class TabComponent {
-  create(): String
-}
-class TableComponent {
-  create(): String
-}
-class ButtonComponent {
-  create(): String
-}
-class InputComponent {
-  create(): String
-}
-class MessageView {
-  WARNING: 1
-  SUCCESS: 2
-  DANGER: 3
-  render() : String
-}
-Expression <|.. Money
-Expression <|.. Sum
-Money <- Sum
-Sum <- Bank
-Bank -> Pair
-Bank <-- Report
-Sum <-- Report
-Money <-- ReportLineItem
-ExChangeRates o- ExChangeRate
-ReportService -o MoneyService 
-MoneyService o- ExChangeRateService
-ExChangeRates <-- ExChangeRateService
-ExChangeRate <-- ExChangeRateRepository
-ReportRepository -o ReportService
-Report <- ReportRepository
-ExChangeRateService o- ExChangeRateRepository
-ReportRepository *-- MoneyDB 
-ExChangeRateRepository *-- MoneyDB 
-ReportLineItem -o Report
-Report <-- ReportService
-Bank <-- ReportService
-MoneyService ---o MoneyView
-MoneyView o-- ExChangeRateView
-MoneyView o-- ReportView
-MoneyView -> TabComponent
-MessageView -o MoneyView
-ReportView --> TableComponent
-ReportView --> ButtonComponent 
-ReportView --> InputComponent
-ExChangeRateView --> TableComponent
-ExChangeRateView --> ButtonComponent
-ExChangeRateView --> InputComponent
-ButtonComponent <|-- DefaultButtonComponent
-ButtonComponent <|-- DefaultMiniButtonComponent
-ButtonComponent <|-- DangerMiniButtonComponent
-InputComponent <|--- FileInputComponent
-InputComponent <|--- TextInputComponent
+Movie "1"<-"*" Rental
+Rental "*"<-"1" Customer
+          `;
+  compress(source, outputId);
+})();
+
+const sequencDiagram = (() => {
+  const inputId = "sequence-diagram-input";
+  const outputId = "sequence-im";
+  const source = `
+-> aCustomer: statement
+   activate aCustomer
+       aCustomer -> aCustomer :*[for all rental]
+       activate aCustomer
+           aCustomer -> aRental :getMovie
+           aCustomer -> aMovie :getPriceCode
+           aCustomer -> aRental :getDaysRented
+       deactivate aCustomer
+   deactivate aCustomer
           `;
   compress(source, outputId);
 })();
